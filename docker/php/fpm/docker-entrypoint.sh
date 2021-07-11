e6e10295bf37931b20cd73ddbe591cca27c81732
Ximbo <mail@ximbo.ru>
2021-07-11 06:56:33 +0300

    Fix fpm configuration for memory limit

--- docker/php/fpm/docker-entrypoint.sh
+++ docker/php/fpm/docker-entrypoint.sh
@@ -12,5 +12,7 @@
 
 # Configure PHP-FPM
 [ ! -z "${MAGENTO_RUN_MODE}" ] && sed -i "s#!MAGENTO_RUN_MODE!#${MAGENTO_RUN_MODE}#" /usr/local/etc/php-fpm.conf
+[ ! -z "${PHP_MEMORY_LIMIT}" ] && sed -i "s#!PHP_MEMORY_LIMIT!#${PHP_MEMORY_LIMIT}#" /usr/local/etc/php-fpm.conf
+[ ! -z "${UPLOAD_MAX_FILESIZE}" ] && sed -i "s#!UPLOAD_MAX_FILESIZE!#${UPLOAD_MAX_FILESIZE}#" /usr/local/etc/php-fpm.conf
 
 exec "$@"
